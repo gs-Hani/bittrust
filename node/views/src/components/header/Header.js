aed0eb7b341cb36caecd5a5769e7bf60e1f3017c
radwa <radwa2484@gmail.com>
2023-05-27 14:12:48 +0300

    first

--- node/views/src/components/header/Header.js
+++ node/views/src/components/header/Header.js
@@ -1,7 +1,7 @@
 import   React,     { useEffect }   from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link }                     from 'react-router-dom';
-
+import './Header.css'
 import { is_Auth }         from '../../features/auth/authSlice';
 
 import Bittrust from '../../util/Bittrust.svg';
@@ -11,18 +11,27 @@ export const Header = () => {
     const   dispatch        = useDispatch();
     const { authenticated } = useSelector(state => state.auth);
 
-    // useEffect (() => { dispatch(is_Auth()); },[dispatch,authenticated]);
+    useEffect (() => { dispatch(is_Auth()); },[dispatch,authenticated]);
 
     const Profile = () => {
         if (authenticated) {
-            return (<Link to='/profile'>Profile</Link>)
+            return (
+                <header className='header'>
+                <div id='links'>
+                  <Link to='/profile'>Profile</Link>
+                  <Link to='/transactions'>Transaction</Link>
+                  <Link to='/'>Sign Out</Link>
+                </div>
+                </header>
+              );
         }
     }
 
     return (
-        <header>
+        <header className='header'>
             <img src={Bittrust} alt={`Logo`}/>
             {Profile()}
-        </header>
+            </header>
+         
     )
 };
