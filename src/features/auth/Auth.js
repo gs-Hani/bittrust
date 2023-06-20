db1012a334a749cdee4ad6625256f1014db0c244
GS Hani <hani_alazawi@hotmail.com>
2023-06-20 18:14:45 +0300

    demo 7

---
+++ src/features/auth/Auth.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector }   from 'react-redux';
+import { useNavigate }                from 'react-router-dom';
+
+import './Auth.css';
+import { sign_up, sign_in }           from './authSlice';
+import { matchPassword }              from '../../util/usefulFunctions';
+
+export const Auth = () => {
+  const [email, setEmail]           = useState();
+  const [password, setPassword]     = useState();
+  const [ref, setRef]               = useState();
+  const [showSignUp, setShowSignUp] = useState(true);
+  const  navigate                   = useNavigate();
+  const  dispatch                   = useDispatch();
+  const { authenticated }           = useSelector((state) => state.auth);
+  const { error1 }                  = useSelector((state) => state.auth);
+  const { status1 }                 = useSelector((state) => state.auth)
+  
+  useEffect(() => { console.log(authenticated);
+    if (authenticated) { navigate('/transRecords'); }
+  }, [authenticated, navigate]);
+
+  const register = async (email, password, ref) => {
+    dispatch(sign_up({ email, password, ref }));
+  };
+
+  const login = (email, password) => {
+    console.log('dispatch sign_in')
+    dispatch(sign_in({ email, password }));
+  };
+
+  const toggleSignUp = () => {
+    setShowSignUp(!showSignUp);
+  };
+
+  const refCodeInput = () => {
+    const url = new URL(document.location);
+    const refCode = url.pathname.split('/')[1];
+    if (!refCode) {
+      return (
+        <input
+          type="text"
+          id="refCode"
+          name="refCode"
+          placeholder="Refferal code"
+          onChange={(e) => setRef(e.target.value)}
+        />
+      );
+    } else {
+      return (
+        <input
+          type="text"
+          id="refCode"
+          name="refCode"
+          value={refCode}
+          readOnly
+        />
+      );
+    }
+  };
+
+  const loading = () => {
+    return (
+      <div className="ring">Loading
+        <span></span>
+      </div>
+    )
+  };
+
+  const auth = () => {
+    return (
+      <div className="auth">
+        {showSignUp ? (
+          <form
+            id="sign-in-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              login(email, password);
+            }}
+          >
+            <h2>Sign IN</h2>
+            <input
+              type="email"
+              id="Email"
+              name="email"
+              placeholder="example@emailprovider.com"
+              onChange={(e) => setEmail(e.target.value)}
+              size="25"
+              autoComplete="on"
+              // required
+            />
+  
+            <input
+              type="password"
+              id="Password"
+              name="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              // minLength="8"
+              maxLength="32"
+              autoComplete="off"
+              // required
+            />
+  
+            <input type="submit" 
+            id="Submit" 
+            value="Log in"
+             />
+  
+            <button type="button" id="slide" onClick={toggleSignUp}>
+              no account?
+            </button>
+          </form>
+        ) : null}
+  
+        {/*========================================================================================*/}
+        {showSignUp ? null : (
+          <form
+            id="sign-up-form"
+            onSubmit={(e) => { e.preventDefault(); register(email, password, ref); }}
+          >
+            <h2>Sign UP</h2>
+  
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="example@emailprovider.com"
+              onChange={(e) => setEmail(e.target.value)}
+              size="25"
+              autoComplete="on"
+              required
+            />
+  
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              onChange={(e) => { matchPassword(e.target.value); setPassword(e.target.value); }}
+              // minLength="8"
+              maxLength="32"
+              autoComplete="off"
+              required
+            />
+    
+            <input
+              type="password"
+              id="confirm-password"
+              placeholder="confirm-password"
+              onChange={(e) => matchPassword(e.currentTarget.value)}
+              autoComplete="off"
+              required
+            />
+    
+            {refCodeInput()}
+    
+            <input type="submit" id="submit" value="Register" />
+            <button type="button" id="slide" onClick={toggleSignUp}>
+              Sign In
+            </button>
+          </form>
+          )}
+    
+          {error1 && <p>{error1}</p>}
+        </div>
+      );
+  }
+
+  return (
+    <div>
+      {status1 === 'loading' ? loading() : auth() }
+    </div>
+    )
+  };
+  
---
+++ src/util/fetch/fetchAuth.js
@@ -0,0 +1,190 @@
+const user1 = {
+  "email": "hani_alazawi@hotmail.com",
+  "id": "2601",
+  "referral_credit": "0",
+  "password": "$2b$17$/j6t2j7BPanmDAEIvadj9udqAvtLDGGNfcE3Dsb1nIL5BRtVv76CO",
+  "deals":[
+    {
+      "id": "6489785185",
+      "amount": "1260",
+      "date": "2021-10-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6708187006",
+      "amount": "6470",
+      "date": "2021-10-20",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7909162680",
+      "amount": "3000",
+      "date": "2022-02-14",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7996716959",
+      "amount": "3125",
+      "date": "2022-02-22",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "8808142120",
+      "amount": "1260",
+      "date": "2022-05-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "9399357857",
+      "amount": "1000",
+      "date": "2022-07-05",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6489785185",
+      "amount": "1260",
+      "date": "2021-10-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6708187006",
+      "amount": "6470",
+      "date": "2021-10-20",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7909162680",
+      "amount": "3000",
+      "date": "2022-02-14",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7996716959",
+      "amount": "3125",
+      "date": "2022-02-22",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "8808142120",
+      "amount": "1260",
+      "date": "2022-05-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "9399357857",
+      "amount": "1000",
+      "date": "2022-07-05",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6489785185",
+      "amount": "1260",
+      "date": "2021-10-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6708187006",
+      "amount": "6470",
+      "date": "2021-10-20",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7909162680",
+      "amount": "3000",
+      "date": "2022-02-14",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7996716959",
+      "amount": "3125",
+      "date": "2022-02-22",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "8808142120",
+      "amount": "1260",
+      "date": "2022-05-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "9399357857",
+      "amount": "1000",
+      "date": "2022-07-05",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6489785185",
+      "amount": "1260",
+      "date": "2021-10-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "6708187006",
+      "amount": "6470",
+      "date": "2021-10-20",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7909162680",
+      "amount": "3000",
+      "date": "2022-02-14",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "7996716959",
+      "amount": "3125",
+      "date": "2022-02-22",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "8808142120",
+      "amount": "1260",
+      "date": "2022-05-04",
+      "dealName": "Buy 500 bits"
+    },
+    {
+      "id": "9399357857",
+      "amount": "1000",
+      "date": "2022-07-05",
+      "dealName": "Buy 500 bits"
+    }
+  ],
+};
+//===================================
+
+const signUp  = async(password,email,ref) => {
+    const res = await fetch(`/auth/signUp`, { 
+        method     : 'POST',
+        credentials: 'include',
+        body       :  JSON.stringify({
+                      email:       email,
+                      password:    password,
+                      referred_by: ref
+        }),
+        headers    : {
+            "Content-Type": "application/json"
+        } 
+    });
+    const  json = await res.json();
+    return json;
+};
+
+const signIn  = async (email,password) => {
+//  console.log('fetching sign in');
+//   const res = await fetch(`/auth/signIn`,{
+//     method     : 'POST', 
+//     credentials: 'include',
+//     body       :  JSON.stringify({ email,password }),
+//     headers    : { "Content-Type": "application/json" } 
+//   });
+//   const  json = await res.json();
+//   console.log('signIn res:',json);
+//   return json;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(user1);
+    }, 2000);
+  });
+};
+
+export { signUp, signIn };
